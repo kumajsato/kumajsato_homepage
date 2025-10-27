7c57103aeb2056f18ffefec4e1822308fb8e01fa
Riku-Y <ptfull1@gmail.com>
2025-10-27 16:57:00 +0900

    Hashrouterに対応したURLを設定

--- src/locales/ja.js
+++ src/locales/ja.js
@@ -19,7 +19,7 @@ const ja = {
     ,{ date: "2025/10/26", content: "音系・メディアミックス同人即売会 [M3]<a href='https://catalog.m3net.jp/2025f/circles.html?circleId=560308' target='_blank'><strong>【E-02a】</strong></a>に出展予定！" }
     ,{ date: "2025/11/29", content: "ボーカロイドonly event「THE VOC@LOiD M@STER６１」<strong>【F08】</strong>にて出展予定！" }
     ,{ date: "2025/10/15", content: "M3、ボーマスにて頒布するCD『アイムタフ feat. 知声 - EP』の情報を公開しました。" }
-    ,{ date: "2025/10/27", content: "<a href='/blog/m3-autumn-2025'>M3秋2025出展報告</a>をブログに追加しました。" }
+    ,{ date: "2025/10/27", content: "<a href='/#/blog/m3-autumn-2025'>M3秋2025出展報告</a>をブログに追加しました。" }
     ,{ date: "2025/10/27", content: "『アイムタフ！ feat. 知声 - EP』のストリーミング配信リンクを追加しました。" }
   ],
 
